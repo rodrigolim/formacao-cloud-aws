489cf269eb058224d7d55e4d414eb8767a734693
Rodrigo de Lima Cunha <rodrigo.cunha@youxgroup.com.br>
2023-02-25 15:45:10 -0300

    Update labels json products

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://demoback-env.eba-22hskfki.us-east-1.elasticbeanstalk.com/api/'
+  url: 'http://apiproduto-env.eba-2wzud2pe.sa-east-1.elasticbeanstalk.com/api/'
 };
